4f969b74b4d07012b8ca05f0a7b2ed3c9d3835bc
liamking0046 <76438483+liamking0046@users.noreply.github.com>
2025-06-06 13:25:44 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    ownerName: "Liam Arendsen",
+    ownerContact: "0833098338",
+    menuGif: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExc.../giphy.gif" // Replace with chosen One Piece GIF
+};
